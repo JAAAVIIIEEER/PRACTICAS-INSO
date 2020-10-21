efa7870ef3e56eb528c60c735786289de601a192
asedac00 <47630673+asedac00@users.noreply.github.com>
2020-10-21 18:31:56 +0200

    Update Cliente.java

--- iso_patternsTODO/src/Cliente.java
+++ iso_patternsTODO/src/Cliente.java
@@ -1,12 +1,18 @@
 
 public class Cliente {
 	
+	private Moneda moneda;
+	private Lista lista;
+	private AbstractFactoryIdiomas factoria;
+	
 	public Cliente(AbstractFactoryIdiomas a) {
-		
+		this.moneda=this.factoria.getMoneda();
+		this.lista=this.factoria.getLista();
+		this.factoria = a;
 	}
 	
 	public void visualizaElems() {
-		
+		moneda.visualizar();
+		lista.visualizar();
 	}
-
 }
